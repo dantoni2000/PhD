92a2dfae27bf589ce77c474c221305877e5d8b1b
dantoni2000 <daniele.toni@sns.it>
2025-03-04 11:36:28 +0100

    Add files via upload

--- rank_1noise.m
+++ rank_1noise.m
@@ -2,14 +2,14 @@
 %on rank (1 + noise) matrices with incoherent singular vectors
 
 n=100;
-ee=1e-10;
+ee=1e-04;
 U=ee*ones(n,1);
 U(randperm(n,10))=1/sqrt(10);
 S=20;
 V=ee*ones(n,1);
 V(randperm(n,10))=1/sqrt(10);
 
-rf=1e-14;
+rf=1e-04;
 E=randn(n,n);
 E=E/norm(E);
 E=rf*E;
@@ -27,7 +27,7 @@ for j=1:1000
     V_c(r,:)=V(r,:);
     AE_c=U*S*V_c';
 
-    if norm(AE-AE_c*pinv(AE_c)*AE)<1e-12
+    if norm(AE-AE_c*pinv(AE_c)*AE)<1e-03
         pb=pb+1;
     end
 end
