33f87a29fde08c3d435a9dfb4b0f1e8f064ce5a0
danitoni2000 <daniele.toni@sns.it>
2025-03-21 22:23:12 +0100

    rank 1 finished!!!

---
+++ MATRIX_RANKONE.m
@@ -0,0 +1,26 @@
+%test 15 03 2025 
+%for a matrix C, we show that there exists Ej so that the bound is tight
+
+
+    c1 = 1e-3; c2 = -0.5e-2; c3 = 4e-4; c4 = -1e-3;
+    % c2 = c1; c3 = c1; c4 = c1;
+    err = 0.004;
+
+    C = [c1; c2; c3; c4];
+    c = norm(C);
+    theta = atan(-c1/c2);
+    R = eye(4,4);
+    R(1:2,1:2) = [cos(theta)  sin(theta);
+                  -sin(theta) cos(theta)];
+   
+    RC = R*C;
+    RE = [sqrt(err^2 -err^4/c^4*RC(2)^2 - err^4/c^4*RC(3)^2 -err^4/c^4*RC(4)^2); -err^2/c^2 * RC(2); -err^2/c^2 * RC(3); -err^2/c^2 * RC(4)];
+    E = R'*RE;
+
+    Ct = R'*(RC+RE);
+    [Q,~] = qr(Ct,0);
+    
+    n1 = norm(Q*Q'*C)^2;
+    n2 = norm(C)^2 - norm(E)^2;
+    
+    m = abs(n1 - n2);
--- TEST_MATRIX.m
+++ TEST_MATRIX.m
@@ -1,15 +1,15 @@
 %test 15 03 2025 
 %for a matrix C having a zero element, we explicitly found Ej so that the bound is tight
 
-T = 1e+6;
+T = 1e+1;
 CC = [];
 
 for r=1:T
 
-    c1 = 1e-1*randn; c2= 1e-1*randn; c=c1^2 + c2^2;
+    c1 = 1e-1; c2= 1e-1; c=c1^2 + c2^2;
     C = [c1; c2; 0];
-    err = 1e-4;
-    E = [-err^2/c * c1; -err^2/c * c2; sqrt(err^2 -err^4/c^2*c1^2 - -err^4/c^2*c2^2)];
+    err = 1.9e-1;
+    E = [-err^2/c * c1; -err^2/c * c2; sqrt(err^2 -err^4/c^2*c1^2 -err^4/c^2*c2^2)];
     
     Ct = C+E;
     [Q,~] = qr(Ct,0);
@@ -25,4 +25,4 @@ for r=1:T
 
 end
 
-max(dif)
+max(dif);
